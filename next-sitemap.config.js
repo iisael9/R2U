f2daabd506042ffa6977c13459d571f23b5ececa
Isael <iisael090@gmail.com>
2025-06-08 16:45:14 -0700

    sitemap url updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://your-site.vercel.app', // Replace with your domain
+  siteUrl: 'https://www.r2u-consulting.com/', // Replace with your domain
   generateRobotsTxt: true, // (optional)
 }
